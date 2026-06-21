c4a2972ba31c73428766fcbf5d53fb9c840618c9
Guhan-afk <gugeliuyiwei@gmail.com>
2026-06-22 00:56:04 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.BIO_DATA_AUDIT_SITE_CONFIG = {
+  feedbackUrl: "https://docs.qq.com/doc/DUmh6Z3J5bUVjTmRh",
+  repoUrl: "https://example.com/replace-with-repository-link",
+  downloadUrl: "./downloads/bio-data-audit-skill.zip"
+};
